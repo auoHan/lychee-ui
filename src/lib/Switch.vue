2871d396e921c1bff909285bcf4993b251732124
auoHan <710543268@qq.com>
2021-12-31 07:22:41 +0800

    hover之后右滚

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-<button><span></span></button>
+  <button><span></span></button>
 </template>
 
 <script lang="ts">
@@ -11,7 +11,7 @@ export default {
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
+button {
   height: $h;
   width: $h*2;
   border: none;
@@ -19,13 +19,18 @@ button{
   border-radius: $h/2;
   position: relative;
 }
-span{
+
+span {
   position: absolute;
   top: 2px;
   left: 2px;
   height: $h2;
   width: $h2;
-  background:white;
+  background: white;
   border-radius: $h2 / 2;
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
 }
 </style>
