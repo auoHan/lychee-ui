885fd8ccf6237b51e8ddd7a9d1fcfe43216b1b9f
auoHan <710543268@qq.com>
2021-12-31 03:03:45 +0800

    封装Topnav组件，添加全局默认样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="top-nav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>荔枝UI</h1>
       <h2>介绍</h2>
@@ -18,33 +12,15 @@
   </div>
 </template>
 
-<script lang="js">
-
+<script lang="ts">
+import Topnav from '../components/Topnav'
 export default {
-  name:'Home'
+  name:'Home',
+  components: {Topnav}
 }
 </script>
 
 <style lang="scss" scoped>
-.top-nav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-
-
 .banner {
   padding: 100px 0;
   display: flex;
