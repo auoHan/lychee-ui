6812689f2c850dda6731a97f37863144010278cb
auoHan <710543268@qq.com>
2021-12-31 02:44:29 +0800

    初始化html和css

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createRouter, createWebHashHistory} from 'vue-router'
-import Han from './components/Han.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 const history = createWebHashHistory()
 const router = createRouter({
   history,
   routes:[
     {
       path:'/',
-      component:Han
+      component:Home
+    },
+    {
+      path:'/doc',
+      component:Doc
     }
   ]
 })
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="top-nav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>荔枝UI</h1>
+      <h2>介绍</h2>
+      <p class="actions">
+        <a >GitHub</a>
+        <a >开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+
+export default {
+  name:'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
