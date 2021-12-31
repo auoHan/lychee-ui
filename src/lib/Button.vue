19e0fc87dab38f8d5c0cb7d3505575fa09b9cbf5
auoHan <710543268@qq.com>
2021-12-31 16:26:24 +0800

    支持loading属性和加载完毕属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -2,6 +2,7 @@
   <button class="lychee-button"
           :class="classes"
           :disabled="disabled">
+    <span v-if="loading" class="lychee-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -27,6 +28,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
   setup(props){
     const {theme,size, level} = props
@@ -171,5 +176,20 @@ $grey: grey;
       color: $grey;
     }
   }
+  > .lychee-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: lychee-spin 1s infinite linear;
+  }
+}
+@keyframes lychee-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
