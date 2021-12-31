f665533150e72580a0642cb678be8b2a3f32de8c
auoHan <710543268@qq.com>
2021-12-31 10:00:33 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,11 +2,17 @@
   <div>
     Button 示例
   </div>
+  <h1>实例1</h1>
+  <div>
+    <Button>hello</Button>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
 export default {
-  name:'SwitchDemo'
+  name:'SwitchDemo',
+  components: {Button}
 }
 </script>
 
