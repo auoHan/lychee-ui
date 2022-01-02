d974ac9764f5d7e7b59efe42558a4116b36cbad2
auoHan <710543268@qq.com>
2022-01-02 10:21:30 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
            v-for="(t,index) in titles"
            :key="index"
            @click="select(t)"
-           :ref="el => { if (el) navItems[index] = el }">{{ t }}
+           :ref="el => { if (t===selected) selectedItem = el }">{{ t }}
       </div>
       <div class="lychee-tabs-nav-indicator"  ref="indicator"></div>
     </div>
@@ -29,23 +29,20 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([])
+    const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      console.log(result)
       const {
         width
-      } = result.getBoundingClientRect()
+      } = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {
         left: left1
       } = container.value.getBoundingClientRect()
       const {
         left: left2
-      } = result.getBoundingClientRect()
+      } = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -68,7 +65,7 @@ export default {
       context.emit('update:selected', title);
     };
     console.log(defaults);
-    return {defaults, titles, select, current,navItems, indicator, container};
+    return {defaults, titles, select, current,selectedItem, indicator, container};
   }
 };
 </script>
