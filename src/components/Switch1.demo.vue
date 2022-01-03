913e07611dc31a826e7ba965fd2005f3dc3131fd
auoHan <710543268@qq.com>
2022-01-03 22:41:42 +0800

    展示源代码

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,27 @@
+<demo>
+常规用法
+</demo>
+<template>
+  <Switch v-model:value="bool" />
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+export default {
+  name:'Switch1.demo',
+  components: {
+    Switch,
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,27 @@
+<demo>
+支持 disabled
+</demo>
+<template>
+  <Switch v-model:value="bool" disabled />
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+export default {
+  name:'Switch2.demo',
+  components: {
+    Switch,
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
